1af7310545f307543cb0c15654975e74717a6608
unknown <igopopov@gmail.com>
2023-05-24 11:06:45 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,50 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'John',
+    lastname: 'Smith',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: {
+    value: '600',
+    currency: 'USD',
+    period: 'per month',
+  },
+  address: 'Kyiv, Ukraine',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'john_smith@mail.com',
+      href: 'mailto:john_smith@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/john_smith-test',
+    },
+  },
+}
+
+var name = [
+  { name: 'University of Oxford', isEnd: true },
+  { name: 'University of Cambridge', isEnd: true },
+  { name: 'Yale University', isEnd: false },
+  { name: 'University of Paris', isEnd: false },
+]
+
+var certificates = [
+  { name: 'Diploma of a Master degree', id: 257 },
+  { name: 'Diploma of a Bachlor degree', id: 356 },
+  { name: 'Diploma of a Genius degree', id: 125 },
+]
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,6 +67,141 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Summary',
+    },
+    header,
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
+      },
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+    footer,
+  })
+})
+
+router.get('/skills', function (req, res) {
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Skills',
+    },
+    header,
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'VS Code & NPM',
+          point: 9,
+          isTop: false,
+        },
+        {
+          name: 'Git & Terminal',
+          point: 8,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        { main: 'Diving', isMain: true },
+        { main: 'Climbing', isMain: false },
+        { main: 'Music', isMain: true },
+        { main: 'Reading', isMain: true },
+        { main: 'Swimming', isMain: true },
+      ],
+    },
+    footer,
+  })
+})
+
+router.get('/education', function (req, res) {
+  res.render('education', {
+    page: {
+      title: 'Resume | Educations',
+    },
+    header,
+    main: { name, certificates },
+    footer,
+  })
+})
+
+router.get('/work', function (req, res) {
+  res.render('work', {
+    layout: 'big',
+    page: {
+      title: 'Resume | Work',
+    },
+    header,
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.03.2023',
+            to: null,
+          },
+          projectAmount: 3,
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua/',
+              about:
+                'Airbnb competitor. High-load application for searching apartments',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Best Public Sector Project',
+                },
+                {
+                  name: 'Security Innovation In Banking Applications',
+                },
+              ],
+              stackAmount: 5,
+              awardAmount: 3,
+            },
+          ],
+        },
+      ],
+    },
+    footer,
   })
 })
 
